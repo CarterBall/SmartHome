eee4489bbca067827b24563e710b28987c944c96
travoloney <travismbolster@gmail.com>
2025-11-01 06:36:56 -0700

    made some tweaks and comments to thermometer class and created a humidity class that works similar to the thermometer

--- code/scripts/humidity.py
+++ code/scripts/humidity.py
@@ -1,18 +1,18 @@
 import requests
 
 
-class thermometer:
+class humidity:
 
     # thermometer constructor
-    def __init__(self, tempEntityID, humidityEntityID, homeAssistantUrl, accessToken):
-        self.tempEntityID = tempEntityID  # which thermometer to connect to
+    def __init__(self, humidityEntityID, homeAssistantUrl, accessToken):
+        self.humidityEntityID = humidityEntityID  # which Humidity reader to connect to
         self.homeAssistantUrl = homeAssistantUrl  # where the home assitant live
         self.accessToken = accessToken  # proving Identity
 
     # Sends a request given a entityID.
     def sendRequest(self):
         # send request to this location
-        URL = self.homeAssistantUrl + "/api/states/" + self.tempEntityID
+        URL = self.homeAssistantUrl + "/api/states/" + self.humidityEntityID
         # authorization step
         headers = {
             "Authorization": "Bearer " + self.accessToken,
@@ -26,16 +26,9 @@ class thermometer:
             print("Failed to get response from lights")
             return None
 
-    # Returns temp in celcius which is default.
-    def celsius(self):
-        temp = self.sendRequest()
-        if temp is not None:
-            print(f"Temperature: {temp}°C")
-        return temp
-
-    # Returns fahrenheit after converting from celcius
-    def fahrenheit(self):
-        temp = self.sendRequest()
-        if temp is not None:
-            print(f"Temperature: {temp * 9/5 + 32}°F")
-        return temp
+    # Returns the humidity
+    def humidity(self):
+        humidity = self.sendRequest()
+        if humidity is not None:
+            print(f"Humidity: {humidity}%")
+        return humidity
